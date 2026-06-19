162a0d68f3da3f4ecc7244428621f3631a0839ea
VERT <i.a.zakrevskii@gmail.com>
2026-06-19 20:37:29 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,10 @@
-def calculate_average(grades):          # объявляем функцию, которая считает средний балл
-    if not grades:                      # если список оценок пустой
-        return 0.0                      # возвращаем 0.0
-    return round(sum(grades) / len(grades), 2)   # считаем среднее и округляем до 2 знаков
-
-
-def get_attendance_rate(attendance):    # функция считает процент посещаемости
-    if not attendance:                  # если список посещаемости пустой
+def calculate_average(grades): #функция считает средний балл
+    if not grades:
         return 0.0
-    attended = sum(1 for x in attendance if x)   # считаем сколько True (был на занятии)
-    return round((attended / len(attendance)) * 100, 1)  # считаем процент и округляем
+    return round(sum(grades) / len(grades), 2)
+
+def get_attendance_rate(attendance): #функция считает процент посещаемости
+    if not attendance:
+        return 0.0
+    attended = sum(1 for x in attendance if x)
+    return round((attended / len(attendance)) * 100, 1)
