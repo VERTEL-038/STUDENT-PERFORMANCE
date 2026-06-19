d310a10e22c44ad51d9ce5846be2f58740822ff6
VERT <i.a.zakrevskii@gmail.com>
2026-06-19 20:39:12 +0300

    Refactor show_menu and show_test_data functions

--- ui.py
+++ ui.py
@@ -1,16 +1,13 @@
 from data import students, classes
 
-def show_menu():
+def show_menu(): #функция выводит меню для взаимодействия
     print("─" * 80)
     print("ГЕНЕРАТОР ОТЧЕТОВ ОБ УСПЕВАЕМОСТИ СТУДЕНТОВ\n")
     print("1. Показать отчет")
     print("2. Показать тестовые данные")
     print("0. Выход")
 
-
-def show_test_data():
-    """Вывод тестовых данных"""
-
+def show_test_data(): #функция выводит тестовые данные
     print("─" * 80)
     print("Тестовые данные студентов:\n")
     for s in students:
